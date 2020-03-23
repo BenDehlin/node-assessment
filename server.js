20edd434b5ca337ec2ae0da518bce7de6d4b5174
Benjamin Dehlin <ben.dehlin@gmail.com>
2020-03-23 14:20:51 -0600

    boilerplate

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const app = express()
+const port = 3000
+const userCtrl = require('./usersCtrl')
+
+
+app.use(express.json())
+
+app.get('/api/user', userCtrl.getUsers)
+app.get('/api/user/:userId', userCtrl.getUser)
+app.get('/api/admin', userCtrl.getAdmins)
+app.get('/api/nonadmin', userCtrl.getNonAdmins)
+app.get('/api/type/:userType', userCtrl.getUserTypes)
+
+app.put('/api/user/:userId', userCtrl.editUser)
+
+app.post('/api/user', userCtrl.createUser)
+
+app.delete('/api/user/:userId', userCtrl.deleteUser)
+
+app.listen(port, () => console.log(`Server running on ${port}`))
---
+++ usersCtrl.js
@@ -0,0 +1,12 @@
+const userData = require('./userData.json')
+
+module.exports = {
+  getUsers: (req, res) => {},
+  getUser: (req, res) => {},
+  getAdmins: (req, res) => {},
+  getNonAdmins: (req, res) => {},
+  getUserTypes: (req, res) => {},
+  editUser: (req, res) => {},
+  createUser: (req, res) => {},
+  deleteUser: (req, res) => {}
+}
